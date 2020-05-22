5b7cefc7f19968750d7ba472bc2f2cf009b6808a
Lucas Ospina Aristizabal <lucas.ospina.a@multienlace.com.co>
2020-05-22 11:57:08 -0500

    Scope changed

--- src/reducers/city.js
+++ src/reducers/city.js
@@ -3,7 +3,7 @@ import { SET_CITY } from '../actions'
   export const city = (state = {}, action) => {
       switch (action.type) {
           case SET_CITY:
-            return{...state, city: action.payload};
+            return action.payload; ///se cambia porque ya city esta declarado desde el combine reducer 
           default:
               return state;
       }
