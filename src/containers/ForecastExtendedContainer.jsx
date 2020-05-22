b4f9445282acfa67d87e7a18dd059484fb82e26f
Lucas Ospina Aristizabal <lucas.ospina.a@multienlace.com.co>
2020-05-22 11:59:18 -0500

    Adding state forecast data

--- src/containers/ForecastExtendedContainer.jsx
+++ src/containers/ForecastExtendedContainer.jsx
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import ForecastExtended from '../components/ForecastExtended';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import ForecastExtended from "../components/ForecastExtended";
 
 class ForecastExtendedContainer extends Component {
-    render() {
-        return (
-            this.props.city &&
-                <ForecastExtended city= {this.props.city}/>
-        );
-    }
+  render() {
+    const { city, forecastData } = this.props;
+    return city && <ForecastExtended city={city} forecastData={forecastData} />;
+  }
 }
 
 ForecastExtendedContainer.propTypes = {
-    city : PropTypes.string.isRequired
-}
+  city: PropTypes.string.isRequired,
+  forecastData: PropTypes.array.isRequired,
+};
 
-const mapStateToProps = ({city}) => ({city})
-export default connect(mapStateToProps,null)(ForecastExtendedContainer);
+const mapStateToProps = ({ city, cities }) => ({
+  city,
+  forecastData: cities[city] && cities[city].forecastData,
+});
+export default connect(mapStateToProps, null)(ForecastExtendedContainer);
